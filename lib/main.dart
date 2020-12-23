c8ea83e989b6cd007fa1925e7a20183194f60fc0
nitishgarg2002 <nitishgarg2002@gmail.com>
2020-12-23 12:49:28 +0530

    cheat and normal scenerio

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:camera_model/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
